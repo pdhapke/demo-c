17de190d637b23e1e4fb3e5d4386210521f70126
pdhapke <peterdhapke@gmail.com>
2023-10-22 22:07:20 -0500

    redirect

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/demo-c'
+    basePath: '/demo-c',
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/demo-c',
+            permanent: true,
+          }
+        ]
+      },
 }
 
 module.exports = nextConfig
